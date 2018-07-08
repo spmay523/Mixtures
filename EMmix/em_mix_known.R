4e98793c62e218560031a68af83e47fc50c959a6
spmay523 <spmay523@gmail.com>
2018-07-07 21:00:07 -0600

    implemented new ability to handle single column x data

--- EMmix/em_mix_known.R
+++ EMmix/em_mix_known.R
@@ -5,50 +5,84 @@
 #' @param threshold numeric vector of length 1 indicating when to terminate the estimation procedure
 #' @param dat data.frame object for frequencies by each population
 #' @param path gives a file path name for the directory where tables of outputs from the iterations should be written
-#' @param Ntot total number of population
-#' @param N_pop1 number of population 1
+#' @param Ntot total number of population considered for x.
 #' @return Numeric vector of mixtures for each population included as a row in pnames
 #' @export
-em_mix_known <- function(x, dat, pnames, pi_init, threshold = 0.01, MAF_thresh = 0.05, path = NA){
-  pi_out <- pi_out_median <- pi_out_90 <- pi_new <- pi_median <- pi_90 <- pi_init
-  names(pi_median) <- names(pi_new) <- names(pi_90) <- pnames[,1]
+em_mix_known <- function(x, dat, pnames, pi_init, Ntot, threshold = 0.01, MAF_thresh = 0.05, path = NA){
+  pi_out <- pi_out_median <- pi_out_90 <- pi_new <- pi_median <- pi_90 <- pi <- pi_init
+  names(pi_median) <- names(pi_new) <- names(pi_90) <- names(pi) <- pnames[,1]
   k <- nrow(pnames)
   N <- nrow(x)
   thresh_check <- threshold + 1
   cols_toSelect <- character(); apply(as.matrix(pnames[,2:ncol(pnames)]), 1, function(pop){cols_toSelect <<- c(cols_toSelect, pop)} )
   p <- dat # p <- dat(which(x$AF > MAF_thresh & x$AF < (1-MAF_thresh)))
   iter = 0
-  
-  # expectation step
-  while(thresh_check > threshold){
-    pi = pi_new
-    #new create gamma_tmp
-    gamma_tmp.a <- cbind(
-      apply(pnames, 1, function(pop){
-        x.p <- cbind(x, p[,pop[2:ncol(pnames)]])
-        apply(x.p, 1, function(gen){
-          pi[pop[1]]*dmultinom(as.numeric(gen[1:(ncol(pnames)-1)]), prob = gen[ncol(pnames):(2 * (ncol(pnames)-1))], log = T)
+  if(ncol(pnames) < 2){stop("something is wrong with the dimensions of the value provided for pnames")}
+  if(ncol(pnames)>2){
+    while(thresh_check > threshold){
+      pi = pi_new
+      # expectation step
+      gamma_tmp.a <- cbind(
+        apply(pnames, 1, function(pop){
+          x.p <- cbind(x, p[,pop[2:ncol(pnames)]])
+          apply(x.p, 1, function(gen){
+            pi[pop[1]]*dmultinom(as.numeric(gen[1:(ncol(pnames)-1)]), prob = gen[ncol(pnames):(2 * (ncol(pnames)-1))], log = T)
+          })
         })
-      })
-    )
-    gamma_tmp<-(apply(gamma_tmp.a,1,function(x){sum(x, na.rm=T)})-gamma_tmp.a)/apply(gamma_tmp.a,1,function(x){sum(x, na.rm=T)})
-    
-    pi_new<-apply(gamma_tmp,2,function(x){mean(x, na.rm=T)})
-    names(pi_new) <- names(pi)
-    pi_median<-apply(gamma_tmp,2,function(x){median(x, na.rm=T)})
-    pi_90<-apply(gamma_tmp,2,function(x){quantile(x, probs=0.90,na.rm=T)})
-    
-    pi_out<-rbind(pi_out, pi_new)
-    pi_out_median<-rbind(pi_out_median, pi_median)
-    pi_out_90<-rbind(pi_out_90, pi_90)
-    iter=iter+1
-    thresh_check<-sum(abs(pi-pi_new))
-    # the write to file is currently not implemented; this will likely need to be deleted
-    # if(!is.na(path)){
-    #   if(round(iter/10)==(iter/10)){
-    #     write.table(gamma_tmp, paste(path,"/pops", names(pi)[1], N_pop1, "_", names(pi)[2], (N-N_pop1), "_start", paste(pi_start, collapse="_"), "_iter", iter, ".txt",sep=""), row.names=F, col.names=F, quote=F, sep="\t")
-    #   }
-    # }
+      )
+      gamma_tmp<-(apply(gamma_tmp.a,1,function(x){sum(x, na.rm=T)})-gamma_tmp.a)/apply(gamma_tmp.a,1,function(x){sum(x, na.rm=T)})
+      
+      # maximization step
+      pi_new<-apply(gamma_tmp,2,function(x){mean(x, na.rm=T)})
+      names(pi_new) <- names(pi)
+      pi_median<-apply(gamma_tmp,2,function(x){median(x, na.rm=T)})
+      pi_90<-apply(gamma_tmp,2,function(x){quantile(x, probs=0.90,na.rm=T)})
+      
+      pi_out<-rbind(pi_out, pi_new)
+      pi_out_median<-rbind(pi_out_median, pi_median)
+      pi_out_90<-rbind(pi_out_90, pi_90)
+      iter=iter+1
+      thresh_check<-sum(abs(pi-pi_new))
+      # the write to file is currently not implemented; this will likely need to be deleted
+      # if(!is.na(path)){
+      #   if(round(iter/10)==(iter/10)){
+      #     write.table(gamma_tmp, paste(path,"/pops", names(pi)[1], N_pop1, "_", names(pi)[2], (N-N_pop1), "_start", paste(pi_start, collapse="_"), "_iter", iter, ".txt",sep=""), row.names=F, col.names=F, quote=F, sep="\t")
+      #   }
+      # }
+    }
+  }
+  # This accounts for instances when x is N x 1 (and pnames is therefore only K x 2); x is N x 1 because the one column is allele frequency
+  if(ncol(pnames) = 2){
+    if(any(x[,1] < 1)){x[,1] <- round(x[,1] * 2 * Ntot)} # need x data as counts for dbinom function
+    while(thresh_check > threshold){
+      pi = pi_new
+      # expectation step
+      gamma_tmp.a <- cbind(
+        apply(pnames, 1, function(pop){
+          x.p <- cbind(x, p[,pop[2:ncol(pnames)]])
+          pi[pop[1]]*dbinom(x = x.p[,1], size = Ntot, prob = x.p[,2], log = TRUE) # this only works when x is given as integers and not frequencies
+        })
+      )
+      gamma_tmp<-(apply(gamma_tmp.a,1,function(x){sum(x, na.rm=T)})-gamma_tmp.a)/apply(gamma_tmp.a,1,function(x){sum(x, na.rm=T)})
+      
+      # maximization step
+      pi_new<-apply(gamma_tmp,2,function(x){mean(x, na.rm=T)})
+      names(pi_new) <- names(pi)
+      pi_median<-apply(gamma_tmp,2,function(x){median(x, na.rm=T)})
+      pi_90<-apply(gamma_tmp,2,function(x){quantile(x, probs=0.90,na.rm=T)})
+      
+      pi_out<-rbind(pi_out, pi_new)
+      pi_out_median<-rbind(pi_out_median, pi_median)
+      pi_out_90<-rbind(pi_out_90, pi_90)
+      iter=iter+1
+      thresh_check<-sum(abs(pi-pi_new))
+      # the write to file is currently not implemented; this will likely need to be deleted
+      # if(!is.na(path)){
+      #   if(round(iter/10)==(iter/10)){
+      #     write.table(gamma_tmp, paste(path,"/pops", names(pi)[1], N_pop1, "_", names(pi)[2], (N-N_pop1), "_start", paste(pi_start, collapse="_"), "_iter", iter, ".txt",sep=""), row.names=F, col.names=F, quote=F, sep="\t")
+      #   }
+      # }
+    }
   }
   iter_results <- cbind(pi_out, pi_out_median, pi_out_90)
   dimnames(iter_results) <- list(c(),c(
